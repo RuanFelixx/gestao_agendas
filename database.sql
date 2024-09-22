f44619b9e9a4bae6d7fe9381bd909d83dd98ed74
RuanFelixx <162609965+RuanFelixx@users.noreply.github.com>
2024-09-22 12:48:15 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,26 +1,27 @@
-create database db_agenda;
-use db_agenda;
+CREATE DATABASE IF NOT EXISTS 'db_agenda';
+USE 'db_agenda';
 
-create table tb_usuarios(
-usu_id int primary key not null auto_increment,
-usu_nome varchar(45) not null,
-usu_senha varchar(255) not null,
-usu_email varchar(100) not null
+CREATE TABLE IF NOT EXISTS tb_usuarios (
+    usu_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    usu_nome VARCHAR(45) NOT NULL,
+    usu_senha VARCHAR(255) NOT NULL,
+    usu_email VARCHAR(100) NOT NULL UNIQUE
 );
 
-create table tb_categoria_tarefas(
-cat_id int primary key not null auto_increment,
-cat_nome varchar(255) not null
+CREATE TABLE IF NOT EXISTS tb_categoria_tarefas (
+    cat_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cat_nome VARCHAR(255) NOT NULL UNIQUE
 );
 
-create table tb_tarefas(
-tar_id int primary key not null auto_increment,
-tar_nome varchar(45) not null,
-tar_descricao varchar(200) not null,
-tar_entrega date,
-tar_cat_id int not null,
-foreign key(tar_cat_id) references tb_categoria_tarefas(cat_id),
-tar_usu_id int not null,
-foreign key(tar_usu_id) references tb_usuarios(usu_id)
-)
+CREATE TABLE IF NOT EXISTS tb_tarefas (
+    tar_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    tar_nome VARCHAR(45) NOT NULL,
+    tar_descricao VARCHAR(200) NOT NULL,
+    tar_entrega DATE,
+    tar_cat_id INT NOT NULL,
+    FOREIGN KEY (tar_cat_id) REFERENCES tb_categoria_tarefas(cat_id),
+    tar_usu_id INT NOT NULL,
+    FOREIGN KEY (tar_usu_id) REFERENCES tb_usuarios(usu_id)
+);
+
 

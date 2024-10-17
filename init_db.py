9edb671bbf99a21020eb065963157b76e6e4594f
RuanFelixx <162609965+RuanFelixx@users.noreply.github.com>
2024-10-17 19:08:32 -0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'mysql'  
+app.config['MYSQL_DB'] = 'db_agenda'  
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 conexao = MySQL(app)
